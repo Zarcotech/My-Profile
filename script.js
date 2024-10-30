5d1cc92525ebe0fba062410d551ee00d8efbc809
Zidan Harb <zidan.harb2012@gmail.com>
2024-10-30 18:16:39 -0500

    new code

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const numRaindrops = 100;
 for (let i = 0; i < numRaindrops; i++) {
     const raindrop = document.createElement('div');
     raindrop.classList.add('raindrop');
-    const randomX = Math.random() * 100; // Random horizontal position
-    const randomDelay = Math.random() * 2; // Random animation delay
+    const randomX = Math.random() * 100;
+    const randomDelay = Math.random() * 2;
 
     raindrop.style.left = `${randomX}vw`;
-    raindrop.style.animationDuration = `${Math.random() * 6 + 1}s`; // Random duration between 1s and 3s
-    raindrop.style.animationDelay = `-${randomDelay}s`; // Random delay
+    raindrop.style.animationDuration = `${Math.random() * 6 + 1}s`;
+    raindrop.style.animationDelay = `-${randomDelay}s`;
 
     document.body.appendChild(raindrop);
 }
